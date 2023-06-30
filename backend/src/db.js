b3ba5d80cadb3810047d81544af2d74134e1fbfe
TerJ <teranj1902@gmail.com>
2023-06-30 03:24:39 -0500

    -

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,10 +2,10 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
-  password: "12345678",
+  password: "123",
   host: "localhost",
   port: 5432,
-  database: "DBD2",
+  database: "PC4",
 });
 
 module.exports = pool;
